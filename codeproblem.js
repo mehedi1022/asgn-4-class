a27e9d080640d13cb8079c136765086283535ac8
mehedi1022 <mahdibinalamgir@gmail.com>
2024-09-02 10:07:07 +0600

    added math topic

--- codeproblem.js
+++ codeproblem.js
@@ -198,5 +198,26 @@ array has some duplicate elements
 // const uniqueArray = noDuplicate(biryanikhor);
 // console.log(uniqueArray);
 
+//6
+// const Math = {
+//   min: function min(num1, num2){
+//     return num2;
+//   }
+// }
+// console.log(Math.min);
+
+// const min = Math.min(45, 21, 65, 99, 126, 5, -99)
+// console.log(min);
+// const max = Math.max(45, 21, 65, 99, 126, 5, -99, 987);
+// console.log(max);
+// console.log(Math.PI);
+// console.log(Math.abs(5-10));
+// console.log(Math.round(2.56));
+// console.log(Math.floor(2.96));
+// console.log(Math.ceil(2.001));
+console.log(Math.random());
+console.log(Math.random()*10);
+const rend = Math.round(Math.random()*10)
+console.log(rend);
 
 
